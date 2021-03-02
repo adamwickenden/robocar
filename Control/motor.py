682a66ed8e95c0abae49de5f29cc0bb5969301b6
Adam <adamwickenden94@gmail.com>
2021-03-02 19:24:26 +0000

    initial commit of modified coce

---
+++ Control/motor.py
@@ -0,0 +1,76 @@
+import time
+
+from board import SCL, SDA # represents clock channel and data channel
+import busio
+
+from adafruit_motor.motor import DCMotor
+from adafruit_pca9685 import PCA9685
+
+from data import motors
+
+freq = 50
+
+class Motor:
+	def __init__(self):
+		# configure i2c connections
+		i2c = busio.I2C(SCL, SDA)
+		# create PCA9685 class instance and supply it with the i2c connections
+		self.pca = PCA9685(i2c)
+		self.pca.frequency = freq
+		# Init refrence dict to servo objects so we can reference them later
+		self.motor_dict = self._init_motors()
+		
+	def _init_motors(self):
+		motor_dict = {}
+		for motor in motors:
+			motor_dict[motor] = DCMotor(self.pca.channels[motors[motor]["+"]], self.pca.channels[motors[motor]["-"]])
+		return motor_dict
+
+	def drive(self, power):
+		# +ve for forward, -ve for backwards
+		for motor in self.motor_dict:
+			self.motor_dict[motor].throttle = power
+
+			
+	def left(self, power):
+		for motor in self.motor_dict:
+			# To turn left we need to apply throttle on the right wheels
+			if 'R' in motor:
+				self.motor_dict[motor].throttle = power
+			else:
+				# set the opposite side to reverse
+				self.motor_dict[motor].throttle = -power
+				
+	def right(self, power):
+		for motor in self.motor_dict:
+			# To turn right we need to apply throttle on the left wheels
+			if 'L' in motor:
+				self.motor_dict[motor].throttle = power
+			else:
+				# set the opposite side to reverse
+				self.motor_dict[motor].throttle = -power
+				
+	def stop(self):
+		for motor in self.motor_dict:
+			self.motor_dict[motor].throttle = 0
+
+
+if __name__ == '__main__':
+	print("Test driving.")
+	motors = Motor()
+	while True:
+		try:
+			motors.drive(0.5)
+			time.sleep(1)
+			motors.drive(-0.5)
+			time.sleep(1)
+			motors.left(0.5)
+			time.sleep(1)
+			motors.right(0.5)
+			time.sleep(1)
+			motors.stop()
+			break
+		except KeyboardInterrupt:
+			print('\n Ended')
+			break
+
---
+++ Control/servo.py
@@ -0,0 +1,47 @@
+from board import SCL, SDA # represents clock channel and data channel
+import busio
+
+from adafruit_motor import servo
+from adafruit_pca9685 import PCA9685
+
+from data import servos
+
+freq = 50
+active_servos = [0, 1]
+
+class Servo:
+	def __init__(self):
+		# configure i2c connections
+		i2c = busio.I2C(SCL, SDA)
+		# create PCA9685 class instance and supply it with the i2c connections
+		self.pca = PCA9685(i2c)
+		self.pca.frequency = freq
+		# Init refrence dict to servo objects so we can reference them later
+		self.servo_dict = self._init_servos()
+		
+	def _init_servos(self):
+		# Create dict of servo objects for easy access
+		servo_dict = {}
+		for channel in active_servos:
+			servo_dict[channel] = servo.Servo(self.pca.channels[servos[channel]], min_pulse=580, max_pulse=2480)
+		return servo_dict
+		
+	def setServoAngle(self, channel, angle):
+		channel = int(channel)
+		if channel in active_servos:
+			print(f"setting servo {channel} using pca channel {servos[channel]} to {angle} degrees")
+			self.servo_dict[channel].angle = angle
+		else:
+			print(f"Incorrect servo channel passed, use {active_servos[0]}-{active_servos[1]}")
+
+
+if __name__ == '__main__':
+	print("Aligning all servos to 90°.")
+	pwm = Servo()
+	while True:
+		try:
+			pwm.setServoAngle(0, 120)
+			pwm.setServoAngle(1, 120)
+		except KeyboardInterrupt:
+			print('\n Ended')
+			break
